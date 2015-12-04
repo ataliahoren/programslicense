ba738d7de9a8d969fcd11963581962a8522eab77
ataliahoren <ataliahoren@gmail.com>
2015-12-05 01:10:16 +0200

    first upload

---
+++ Home.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Program license-home</title>
+	<link rel="stylesheet" href="includes/style.css">
+	<script src="includes/jquery-1.11.3.min.js"></script>
+	<script src="includes/script.js"></script>
+</head>
+	<body>
+		<div id="wrapper">
+			<header>
+					<a href="#" id="logo" alt="logo" title="logo" /></a>
+			</header>
+			<nav id="menu">
+				<ul>
+					<li><a href="#" target="_blank">Licenses</a> </li>
+					<li><a href="License_notifications">Notifications</a> </li>
+					<li><a href="License_reports">Reports</a> </li>
+					<div class="clear"></div>
+				</ul>
+			 </nav>
+			 <nav id="sub_menu">
+				<ul>
+					<li><a href="License_select.php" target="_blank">Select</a></li>
+					<li><a href="License_update.php" target="_blank">Update</a></li>
+					<li><a href="License_create.php" target="_blank">Create</a></li>
+					<div class="clear"></div>
+				</ul>
+			 </nav>
+			 <main> 
+				<section id="content">
+						<h1>Welcom to the Software License System</h1>
+						<h2>In this System you can do:</h2>
+						<ul>
+							<li>manage and update exsisting license</li>
+							<li>create new data</li>
+							<li>getting alerts</li>
+							<li>manage reports</li>
+						</ul>
+				</section> 
+				<aside>
+				
+				</aside> 
+				
+			 </main>
+			 <div class="clear"></div>
+		</div>
+			<footer>
+		 	<p>.Copyright &copy; 
+			<a href="https://il.linkedin.com/in/ataliaschuster" target="_blank">Atalia Schuster&nbsp; |</a>&nbsp;
+			<a href="mailto:ataliahoren@gmail.com">ataliahoren@gmail.com</a> </p>
+			</footer>
+
+</body>
+</html>
---
+++ includes/script.js
@@ -0,0 +1,10 @@
+ $("document").ready(function() {
+ 	 $("#chosen_sub").click(function(){
+ 	 	$("chosen_main").css({"backgrond-color":"url(../images/bgr.jpg)"});
+ 		$("chosen_sub").css({"color":"94b52c"});
+ 	});
+ 	
+ 	/*$("ul #first").mouseover(function(){
+	$("nav #sub_menu").html("<li>hi</li>");
+ 	});	*/
+ });   
